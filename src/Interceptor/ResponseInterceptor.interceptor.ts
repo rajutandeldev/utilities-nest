b3bdb58053a80ce9f2e99e7e8c3d6e0f2404c3f6
raju.thandela <rajuthandela@gmail.com>
2026-03-15 15:39:31 +0530

    handled response

--- src/Interceptor/ResponseInterceptor.interceptor.ts
+++ src/Interceptor/ResponseInterceptor.interceptor.ts
@@ -4,10 +4,24 @@ export class ResponseInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any>{
          return next.handle().pipe(
             map(data=>{
+                let  responseObject:any;
+                if(data && data?.length > 0){
+                    responseObject = {
+                        status: "success",
+                        data:{
+                            list: data,
+                        },
+                        error:null
+                    }
+                }else{
+                    responseObject = {
+                        status: "success",
+                        data,
+                        error:null
+                    }
+                }
                 return {
-                    status: context.switchToHttp().getResponse().statusCode,
-                    data,
-                    error: null
+                    ...responseObject
                 }
             })
          );
