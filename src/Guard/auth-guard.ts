f5ec39410bc5f55ef9c88b9b14bc7476aab20804
raju.thandela <rajuthandela@gmail.com>
2026-03-16 23:33:58 +0530

    fixes

--- src/Guard/auth-guard.ts
+++ src/Guard/auth-guard.ts
@@ -2,6 +2,7 @@ import { CanActivate, ExecutionContext, UnauthorizedException } from "@nestjs/co
 import {JwtService} from "@nestjs/jwt"
 
 export class AuthGuard implements CanActivate {
+    private readonly secretKey = 'sdddsss--ddsss--ddss--ddss--ddss'
     constructor(private jwtService: JwtService) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -12,10 +13,10 @@ export class AuthGuard implements CanActivate {
             throw new UnauthorizedException('No token provided');
         }
 
-        const userData = await this.jwtService.verifyAsync(token);
+        const userData = await this.jwtService.verifyAsync(token, { secret: this.secretKey })
+        console.log('userData', userData)
         if(userData){
             getRequest.user = userData;
-
         }
         return true;
     }
